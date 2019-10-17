b3305c190f3622ce9183ab31ed85c823b388130e
Cody Lee <codylee02@gmail.com>
2019-10-17 14:42:08 -0600

    prepped for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://cody@localhost/noteful"
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://cody@localhost/noteful",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL || "postgresql://cody@localhost/noteful-test"
 };
